f0b6686e198ad27bb53571d7ddcc5756b8bb8682
RikelmySander <rikelmysander@gmail.com>
2025-03-31 20:40:20 -0300

    Convite casamento - commit primeiro desafio

--- interFatecsAula/ConvidadosCasamento.py
+++ interFatecsAula/ConvidadosCasamento.py
@@ -14,11 +14,24 @@ def TabelaVazia():
             print("-" * 20)
 
 def TabelaConjuntos(noivo,noiva):
-    print("-" * 20)
-    for i in "LISTA FINAL", "APENAS NOIVA", "APENAS NOIVO", "POR AMBOS","POR APENAS UM DELES":
-        print(i)
+    for x in "LISTA FINAL", "APENAS NOIVA", "APENAS NOIVO", "POR AMBOS","POR APENAS UM DELES":
         print("-" * 30)
-        print(noivo | noiva)
+        print(x)
+        print("-" * 30)
+        match x:
+            case "LISTA FINAL":
+                result = sorted(noivo | noiva)
+            case "POR AMBOS":
+                result = sorted(noiva & noivo)
+            case "APENAS NOIVA":
+                result = sorted(noiva - noivo)
+            case "APENAS NOIVO":
+                result = sorted(noivo - noiva)
+            case "POR APENAS UM DELES":
+                result = sorted(noivo ^ noiva)
+        print("\n".join(result))
+        if x != "POR APENAS UM DELES":
+            print('*')
 
 while True:
     convidados = input()
@@ -26,11 +39,12 @@ while True:
         TabelaVazia()
         break
     else:
-        nome_convidado, quem_convidou = convidados.split(";")
-        if quem_convidou == "noivo":
-            convidados_noivo.add(nome_convidado)
+        if convidados != 'ACABOU':
+            nome_convidado, quem_convidou = convidados.split(";")
+            if quem_convidou == "noivo":
+                convidados_noivo.add(nome_convidado)
+            else:
+                convidados_noiva.add(nome_convidado)
         else:
-            convidados_noiva.add(nome_convidado)
-
-print(convidados_noivo)
-print(convidados_noiva)
+            break
+TabelaConjuntos(convidados_noivo, convidados_noiva)
